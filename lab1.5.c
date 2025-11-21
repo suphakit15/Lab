f9bc0a91cf1dfc627dc787d523a7c153bfc3b1c8
suphakit15 <mokzung26@gmail.com>
2025-11-21 22:44:50 +0700

    Add files via upload

---
+++ lab1.5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    int evenSum = 0;
+    int oddSum = 0;
+
+    if (scanf("%d", &number) != 1) {
+        return 1;
+    }
+
+    while (number != 0) {
+        if (number % 2 == 0) {
+            evenSum += number;   //คู่
+        } else {
+            oddSum += number;    // คี่
+        }
+
+        if (scanf("%d", &number) != 1) {
+            break;
+        }
+    }
+
+    printf("Even Sum: %d\n", evenSum);
+    printf("Odd Sum: %d\n", oddSum);
+    return 0;
+}
