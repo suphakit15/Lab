10149c19b9e1e37c03c21c5a0c890d4fae04e363
suphakit15 <mokzung26@gmail.com>
2025-11-22 00:43:49 +0700

    Add files via upload

---
+++ lab1.6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int number;
+    
+    
+    do {
+        printf("Enter a number (1-10): ");
+        if (scanf("%d", &number) != 1) {
+            return 1;
+        }
+        if (number <= 0 || number > 10 ){
+            printf("Error: Value must be 1-10\n");
+        }
+           
+    } while (number < 1 || number > 10);
+
+    printf("Input accepted: %d\n", number);
+    return 0;
+}
