17d15c3937acba8314572fd1260f126be01697fd
suphakit15 <mokzung26@gmail.com>
2025-11-24 01:25:57 +0700

    Add files via upload

---
+++ lab2.1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+    int N, i;
+
+    if ( scanf("%d", &N) != 1) {
+        return 1;
+    }
+    if (N < 1 ) {
+        printf(" Error");
+        return 1;
+    }
+
+    for ( i = 0; i < N; i++) {
+        printf("Hello Loop!\n");
+    }
+
+    return 0;
+
+}
