5fefa4e26747b2377baa0287f3de3a44d43bd81a
suphakit15 <mokzung26@gmail.com>
2025-11-24 11:32:41 +0700

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    int clearanceLevel, age, isActive;
+
+
+    if (scanf("%d %d %d", &clearanceLevel, &age, &isActive) != 3 ) {
+        
+        return 1;   
+    }
+    if (clearanceLevel == 3 && isActive == 1 ) { 
+        printf(" Access Grandted"); // สำหรับถูกเงื่อนไขโดยอย่างไดอย่างหนึ่ง
+    }else if(clearanceLevel == 2 && age >=25 && isActive == 1){
+        printf(" Access Grandted");
+    }else{
+        
+        printf(" Access Denied "); // สำหรับคำตอบอื่นๆทั้งหมด
+
+    }
+
+    return 0;
+}
